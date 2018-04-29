962296af85b3818c726f780ab3523cdc5de681d0
memakura <memakura@gmail.com>
2018-04-29 11:35:16 +0900

    Add installer settings

--- s2face.py
+++ s2face.py
@@ -1,29 +1,43 @@
-# s2ext.py
+#!/usr/bin/env python
 # -*- coding: utf-8 -*-
+"""
+ s2face.py
+"""
 
 import asyncio
 from aiohttp import web
 
-import cv2
 import sys
-
-
+import cv2
 
 class S2FACE:
     """ Face detctor class for Scratch 2 extension """
+
     def __init__(self):
-        self.cascPath = sys.argv[1]
+        self.cascPath = 'haarcascades/haarcascade_frontalface_default.xml'
         self.faceCascade = cv2.CascadeClassifier(self.cascPath)
         self.video_capture = cv2.VideoCapture(0)
         self.helper_host = '127.0.0.1'
         self.helper_port = 50212
+
+        self.face_exist = False
         self.face_x = 0
         self.face_y = 0
+        self.ratio = 0
+
+        width = self.video_capture.get(3)
+        #height = self.video_capture.get(4)
+        self.s2_xmax = 240
+        self.s2_ymax = 180
+        self.s2cv_ratio = self.s2_xmax*2 / width
+        #print("width= {}".format(width))
 
     async def run_captureloop(self):
+        """ Video capture loop """
         while True:
             # Capture frame-by-frame
             ret, frame = self.video_capture.read()
+
 
             gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
@@ -32,15 +46,19 @@ class S2FACE:
                 scaleFactor=1.1,
                 minNeighbors=5,
                 minSize=(30, 30)
-            ) #                flags=cv2.cv.CV_HAAR_SCALE_IMAGE
+            )
 
 
-            # Draw a rectangle around the faces
-            for (x, y, w, h) in faces:
-                cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
-
-            self.face_x = int((x + w/2) * 3 / 4 - 240)
-            self.face_y = int(180 - (y + h/2) * 3 / 4)
+            if len(faces)==0:
+                self.face_exist = False
+            else:
+                self.face_exist = True
+                for (x, y, w, h) in faces:
+                    cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
+                    self.face_x = int((x + w/2) * self.s2cv_ratio - self.s2_xmax)
+                    self.face_y = int(self.s2_ymax - (y + h/2) * self.s2cv_ratio)
+                    self.face_width = int(w * self.s2cv_ratio)
+                    self.face_height = int(h * self.s2cv_ratio)
 
             # Display the resulting frame
             cv2.imshow('Video', frame)
@@ -56,6 +74,9 @@ class S2FACE:
         """ Handle polling from Scratch """
         text = "face_x " + str(self.face_x) + "\n"
         text += "face_y " + str(self.face_y) + "\n"
+        text += "face_width " + str(self.face_width) + "\n"
+        text += "face_height " + str(self.face_height) + "\n"
+        text += "face_exist " + ("true" if self.face_exist else "false") + "\n"
         return web.Response(text=text)
 
 
@@ -85,10 +106,10 @@ class S2FACE:
 
         scratch_server = loop.create_server(app.make_handler(), self.helper_host, self.helper_port)
         #web.run_app(app, host='127.0.0.1', port=12345)
-        cap_loop = loop.create_task(self.run_captureloop())
+        #cap_loop = loop.create_task(self.run_captureloop())
         try:
-            loop.run_until_complete(asyncio.wait({cap_loop, scratch_server}))
-            loop.run_forever() # until loop.stop()
+            loop.run_until_complete(asyncio.wait([self.run_captureloop(), scratch_server]))
+            #loop.run_forever() # until loop.stop()
         finally:
             loop.close()
 
---
+++ setup.py
@@ -0,0 +1,85 @@
+# coding=utf-8
+
+import sys
+import json
+import os
+from cx_Freeze import setup, Executable
+
+# --- for resolving KeyError: 'TCL_LIBRARY' ---
+import os.path
+PYTHON_INSTALL_DIR = os.path.dirname(os.path.dirname(os.__file__))
+os.environ['TCL_LIBRARY'] = os.path.join(PYTHON_INSTALL_DIR, 'tcl', 'tcl8.6')
+os.environ['TK_LIBRARY'] = os.path.join(PYTHON_INSTALL_DIR, 'tcl', 'tk8.6')
+# ------
+
+
+name = "s2face"
+version = "0.1"
+description = 'Scratch2 face detector (Open CV)'
+author = 'Hiroaki Kawashima'
+url ='https://github.com/memakura/s2face'
+
+# 変更しない
+upgrade_code = '{9D6BDE76-10D7-4123-B444-54252E8F975F}'
+
+# ----------------------------------------------------------------
+# セットアップ
+# ----------------------------------------------------------------
+shortcut_table = [
+    ("DesktopShortcut",        # Shortcut
+     "DesktopFolder",          # Directory_
+     "s2face",                    # Name
+     "TARGETDIR",              # Component_
+     "[TARGETDIR]s2face.exe",# Target
+     None,                     # Arguments
+     None,                     # Description
+     None,                     # Hotkey
+     None,                     # Icon
+     None,                     # IconIndex
+     None,                     # ShowCmd
+     "TARGETDIR",              # WkDir
+    )
+    ]
+
+# Now create the table dictionary
+msi_data = {"Shortcut": shortcut_table}
+
+build_exe_options = {"packages": ['asyncio', 'idna', 'numpy.core.multiarray'],
+                    "excludes": [],
+                    "includes": [],
+                    "include_files": ['images/','00scratch/', 'haarcascades/']
+}
+#                    "compressed": True
+# Add 'idna' package to resolve ImportError: cannot import name 'idnadata'
+
+bdist_msi_options = {'upgrade_code': upgrade_code,
+                    'add_to_path': False,
+                    'data': msi_data
+}
+
+options = {
+    'build_exe': build_exe_options,
+    'bdist_msi': bdist_msi_options
+}
+
+# exeの情報
+base = None #'Win32GUI' if sys.platform == 'win32' else None
+icon = 'images/icon_256x256.ico'
+
+# exe にしたい python ファイルを指定
+exe = Executable(script='s2face.py',
+                 targetName='s2face.exe',
+                 base=base,
+                 icon=icon
+                 )
+#                 copyDependentFiles = True
+
+# セットアップ
+setup(name=name,
+      version=version,
+      author=author,
+      url=url,
+      description=description,
+      options=options,
+      executables=[exe]
+      )
